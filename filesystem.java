7a517403c219afef5abccbf4809a73142ce8494b
ad <dsa>
2019-11-13 01:02:34 +0300

    Initial commit.

---
+++ filesystem.java
@@ -0,0 +1,5 @@
+
+package ext4fs;
+class filesystem  {
+	
+}
---
+++ main.java
@@ -0,0 +1,15 @@
+import java.util.*;
+import ext4fs;
+package Main;
+class Main  {
+
+	public static void main(String[] args) {
+		while (true){
+			Scanner s=new  Scanner(System.in);
+			String cmd1 = "";
+			cmd1 = s.nextLine();
+			System.out.print(cmd1);		
+		}
+		
+	}
+}
